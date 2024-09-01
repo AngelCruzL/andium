b44de00d4b5db63a3b346c69be72faa80c108296
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-09-01 17:13:18 -0600

    feat(auth): add auth reducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,18 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { authFeatureKey, authReducer } from './features/auth/store/reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore(),
+    provideState(authFeatureKey, authReducer),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
