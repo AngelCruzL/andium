75acd18538dd632921c91722167ea3dac5de7879
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-08-31 20:26:52 -0600

    chore(config): change karma & jasmine to jest & angular testing library

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/',
+    'jest.config.ts',
+    'setup-jest.ts',
+  ],
+};
+
+export default config;
