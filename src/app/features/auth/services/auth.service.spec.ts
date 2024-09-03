90dd89e522d08fb938f0bdd083d22fe06159f2e4
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-09-03 10:31:57 -0600

    feat(auth): add login method at auth service

--- src/app/features/auth/services/auth.service.spec.ts
+++ src/app/features/auth/services/auth.service.spec.ts
@@ -6,7 +6,11 @@ import {
 } from '@angular/common/http/testing';
 
 import { CurrentUser } from '@shared/types';
-import { SignInResponse, SignUpPayload } from '../types';
+import {
+  signInPayloadMock,
+  signInResponseMock,
+  signUpPayloadMock,
+} from '../utils';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -31,28 +35,26 @@ describe('AuthService', () => {
   });
 
   it('should register user successfully', () => {
-    const payload: SignUpPayload = {
-      user: {
-        username: 'testuser',
-        password: 'password123',
-        email: 'test@test.com',
-      },
-    };
-    const response: SignInResponse = {
-      user: {
-        bio: null,
-        image: null,
-        username: 'testuser',
-        token: 'some-token',
-        email: 'test@test.com',
-      },
-    };
     let userResponse: CurrentUser | undefined;
-    service.register(payload).subscribe(user => (userResponse = user));
+    service
+      .register(signUpPayloadMock)
+      .subscribe(user => (userResponse = user));
     const req = httpTestingController.expectOne(req =>
       req.url.endsWith('/users'),
     );
-    req.flush(response);
-    expect(userResponse).toEqual(response.user);
+    req.flush(signInResponseMock);
+
+    expect(userResponse).toEqual(signInResponseMock.user);
+  });
+
+  it('should login user successfully', () => {
+    let userResponse: CurrentUser | undefined;
+    service.login(signInPayloadMock).subscribe(user => (userResponse = user));
+    const req = httpTestingController.expectOne(req =>
+      req.url.endsWith('/users/login'),
+    );
+    req.flush(signInResponseMock);
+
+    expect(userResponse).toEqual(signInResponseMock.user);
   });
 });
